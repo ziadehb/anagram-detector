342e9225fb3b2283ba49cc0282bd12fde1619155
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-19 21:24:04 -0500

    Add files via upload

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+      @word = word
+    end
+
+    def match(word)
+        word.select do|words| 
+            (@word.split("").sort) == (words.split("").sort)
+        end
+    end
+end
